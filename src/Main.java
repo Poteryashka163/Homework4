919590d8654a0f8c51b423b6bad905e22ec0e4e0
Poteryashka163 <scrat0072@gmail.com>
2023-03-15 18:46:56 +0400

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,50 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Решение задачи 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " +age+ " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " +age+ " то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+    public static void task2() {
+        System.out.println("Решение задачи 2");
+        int tempichar = 10;
+        if (tempichar >= 5) {
+            System.out.println("На улице " +tempichar+ " градусов, можно идти без шапки.");
+        } else {
+            System.out.println("На улице " +tempichar+ " градусов, нужно надеть шапку.");
+        }
+    }
+    public static void task3() {
+        System.out.println("Решение задачи 3");
+        int speed = 65;
+        if (speed > 60) {
+            System.out.println("Если скорость " +speed+ " км/ч, то придется заплатить штраф.");
+        } else {
+            System.out.println("Если скорость " +speed+ " км?/ч, то можно ездить спокойно.");
+        }
+    }
+    public static void task4() {
+        System.out.println("Решение задачи 4");
+    }
+    public static void task5() {
+        System.out.println("Решение задачи 5");
+    }
+    public static void task6() {
+        System.out.println("Решение задачи 6");
+    }
+    public static void task7() {
+        System.out.println("Решение задачи 7");
     }
 }
